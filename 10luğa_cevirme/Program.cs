8f2e1380b8d524ccaf629a0d56bf01c6b5c3f1ee
canumut05 <hasozdemirumutcan@gmail.com>
2024-01-12 23:16:33 +0300

    Proje dosyası ekle.

---
+++ 10luğa_cevirme/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ikilik sistemdeki sayıyı giriniz: ");
+        string binaryInput = Console.ReadLine();
+
+        if (long.TryParse(binaryInput, out long sayi))
+        {
+            ConvertBinaryToDecimal(sayi);
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz giriş. Lütfen ikilik sistemde bir sayı girin! ");
+        }
+
+        Console.ReadLine();
+    }
+
+    static void ConvertBinaryToDecimal(long sayi)
+    {
+        long ikiliDeger = sayi;
+        long onluDeger = 0, tabanDeger = 1, basamakDeger;
+
+        while (sayi > 0)
+        {
+            basamakDeger = sayi % 10;
+            onluDeger = onluDeger + basamakDeger * tabanDeger;
+            sayi = sayi / 10;
+            tabanDeger = tabanDeger * 2;
+        }
+
+        Console.WriteLine($"Ikili (Binary) Sayı: {ikiliDeger}");
+        Console.WriteLine($"Onlu (Decimal) Sayı: {onluDeger}");
+    }
+}
